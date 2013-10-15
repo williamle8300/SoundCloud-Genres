7cb877bb95384590f4db33a5f7c3780c1e5d1031
williamle8300 <William@airbook-2.local>
2013-10-15 09:42:54 -0700

    targeting algorithm for 1)freshness and 2)genre-basedness6

--- soundcloudAPI.js
+++ soundcloudAPI.js
@@ -5,14 +5,11 @@
 var clientID = '212b7a5080f5d7f8e831583446771a02'
 var tracks = []
 var monthMark = new Date()
-//var SCmonthMark = '2013-09-14'
+var SCmonthMark = '2013-09-14'
 var input = {query: '', getQuery: function(){return this.query}, setQuery: function(value){this.query = value; filters.tags = this.query} }
 var filters = {
   q: input.getQuery(),
-//  tags: input.getQuery(),
-//  limit: 200, //comment-out to let SC deal the hand
-//  created_at: {'from': SCmonthMark +' 00:00:00'},
-//  filter: 'streamable'
+  created_at: {'from': SCmonthMark +' 00:00:00'}
 }
 
 SC.initialize({
@@ -23,6 +20,12 @@ function getTracks(){
   $('ul.playlist').empty()
   $('ul.playlist').html('<img id="loadGif" src="http://bradsknutson.com/wp-content/uploads/2013/04/page-loader.gif"/>')
   SC.get('/tracks', filters, function(tracks){ //call out to SC servers
+    if(tracks['errors']) {
+      $('ul.playlist').empty()
+      $('ul.playlist').html('<h1>:(</h1>')
+      return
+    }  
+    console.log(typeof tracks)
     var dateDiff = 0, dateDiffTotal = 0, dateDiffPercentile = 0, dateDiffWeighting = 1
     var tempDate
     var playbackCount = 0, playbackTotal = 0, playbackPercentile = 0, playbackWeighting = 1
@@ -56,7 +59,7 @@ function getTracks(){
     console.log(tracks, dateDiffTotal, playbackTotal, favoritingsTotal)
     //sort by rank
     tracks = tracks.sort(sortBy('rank', false, parseInt));
-    tracks = tracks.slice(0,20) //slice from the top
+//    tracks = tracks.slice(0,20) //slice from the top
     //render HTML page
     $('ul.playlist').empty()
     for(var i = 0; i < tracksLen; i++){          //
